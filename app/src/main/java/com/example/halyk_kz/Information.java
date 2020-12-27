fd6453b8ba59b71997630249b57629a58839d296
Aruzhan Kamkat <arukamkat.99@mail.ru>
2020-12-28 03:23:22 +0600

    add class information

---
+++ app/src/main/java/com/example/halyk_kz/Information.java
@@ -0,0 +1,17 @@
+package com.example.halyk_kz;
+
+public class Information {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Information(String title) {
+        this.title = title;
+    }
+}
